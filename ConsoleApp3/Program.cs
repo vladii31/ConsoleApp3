3752149ef6e36611d14b355d010eabdc65e3e524
Admin <Admin@TEB123007>
2024-01-12 14:35:57 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i <= 1400; i +=50) 
+
+                Console.WriteLine( i );
+            Console.ReadKey();  
+        }
+    }
+}
